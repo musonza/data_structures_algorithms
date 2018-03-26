1cba97475d581ae88dcf91793a996f4aa90d0f80
tmusonza <tinashemusonza@gmail.com>
2018-03-25 21:40:15 -0700

    post_fix_eval

--- Stack/Problems/infix_to_postfix.js
+++ Stack/Problems/infix_to_postfix.js
@@ -1,19 +1,20 @@
 const Stack = require('../stack');
 
+const precedence = {
+    "*": 3,
+    "/": 3,
+    "+": 2,
+    "-": 2,
+    "(": 1
+};
+const alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
+
 function infixToPostFix(infixExpr) {
-    const precedence = {
-        "*": 3,
-        "/": 3,
-        "+": 2,
-        "-": 2,
-        "(": 1
-    };
     let opStack = new Stack();
     let postFixList = [];
     let tokenList = infixExpr.split(' ');
     let index = 0;
-    const alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const nums = '0123456789';
 
     while (index < tokenList.length) {
         let token = tokenList[index];
@@ -44,5 +45,44 @@ function infixToPostFix(infixExpr) {
     return postFixList.join(" ");
 }
 
+function postFixEval(postFixExpr) {
+    let operandStack = new Stack();
+    const tokenList = postFixExpr.split(' ');
+    let index = 0;
+
+    while (index < tokenList.length) {
+        let token = tokenList[index];
+        if (nums.includes(token)) {
+            operandStack.push(parseInt(token));
+        } else {
+            let operand2 = operandStack.pop();
+            let operand1 = operandStack.pop();
+            let result = doMath(token, operand1, operand2);
+            operandStack.push(result);
+        }
+
+        index++;
+    }
+
+    return operandStack.pop();
+}
+
+function doMath(op, op1, op2) {
+    if (op == '*') {
+        return op1 * op2;
+    } else if (op == '/') {
+        return op1 / op2;
+    } else if (op == '+') {
+        return op1 + op2;
+    } else {
+        return op1 - op2;
+    }
+}
+
 console.log(infixToPostFix("A * B + C * D")); // A B * C D * +
 console.log(infixToPostFix("( A + B ) * C - ( D - E ) * ( F + G )")); // A B + C * D E - F G + * -
+console.log(postFixEval('7 8 + 3 2 + /')); // 3
+
+let infix = "( 2 + 2 ) * 4 + 2";
+let post_fix = infixToPostFix(infix); // 2 2 + 4 * 2 +
+console.log(postFixEval(post_fix)); // 18
